84923f88a0810aae596ec79b013fc15c9671845a
Kishan Seksaria <kishanseksaria@gmail.com>
2022-01-23 17:00:38 +0530

    Modify User model to include authorization details

--- db.js
+++ db.js
@@ -5,6 +5,9 @@ mongoose.connect('mongodb://localhost:27017/hotel', () => console.log('DB connec
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
+  isOwner: { type: Boolean, default: false },
+  isManager: { type: Boolean, default: false },
+  isReceptionist: { type: Boolean, default: false },
 });
 
 const User = mongoose.model('User', userSchema);
